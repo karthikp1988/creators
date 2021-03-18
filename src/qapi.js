6611cb4833d93633a46ecc96bb522a8629f63d08
karthikp1988 <47483125+karthikp1988@users.noreply.github.com>
2021-03-18 20:11:13 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"605365bccb40ac001fc5ff13",
+				"605365bccb40ac001fc5ff15"
+			]
+		},
+		"605365bccb40ac001fc5ff13": {
+			"id": "605365bccb40ac001fc5ff13",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"605365bccb40ac001fc5ff15": {
+			"id": "605365bccb40ac001fc5ff15",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
